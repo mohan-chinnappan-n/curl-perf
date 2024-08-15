1656db5a1a872b3716430d8fe23e301494b6f7e6
mohan chinnappan <mchinnappan@apple.com>
2024-08-15 15:25:54 -0400

    commnand to run on terminal is given

--- perf3.py
+++ perf3.py
@@ -15,7 +15,22 @@ def run_curl_command(url):
         '-o', '/dev/null', url
     ]
     result = subprocess.run(command, capture_output=True, text=True)
-    return result.stdout
+    return result.stdout, format_curl_command(url)
+
+
+def format_curl_command(url):
+    # Format the curl command with backslashes for readability
+    command = (
+        'curl -s -w \\\n'
+        'time_namelookup:%{{time_namelookup}}\\\\n\\\n'
+        'time_connect:%{{time_connect}}\\\\n\\\n'
+        'time_appconnect:%{{time_appconnect}}\\\\n\\\n'
+        'time_pretransfer:%{{time_pretransfer}}\\\\n\\\n'
+        'time_redirect:%{{time_redirect}}\\\\n\\\n'
+        'time_starttransfer:%{{time_starttransfer}}\\\\n\\\n'
+        'time_total:%{{time_total}} -o /dev/null {}'.format(url)
+    )
+    return command
 
 def parse_curl_output(output):
     # Parse the curl output into a list of tuples
@@ -33,8 +48,11 @@ url = st.text_input("Enter URL", 'https://www.google.com')
 
 if st.button("Fetch Performance"):
     if url:
-        output = run_curl_command(url)
+        output, full_command = run_curl_command(url)
         df = parse_curl_output(output)
         st.dataframe(df)
+        st.subheader("Full curl Command to run on your terminal")
+        st.code(full_command, language='bash')
+        
     else:
         st.error("Please enter a URL.")
